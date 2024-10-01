46d9d6a3df0959d4f1a86eed6542d2f864614873
AlazaziAmr <alazaziamr@gmail.com>
2024-10-02 02:17:11 +0300

    Revert "Init proj"
    This reverts commit da88742df1ebe0a63349e7d86ca9fb2fce93b5a6.

--- website/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class WebSiteConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "website"
-    verbose_name = _("Website Design")
-   
